4613011822681d1e41abfc258ddc4f871c9fed6b
魔人布欧 <1067711620@qq.com>
2020-03-07 07:50:19 +0800

    18.03.01

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -1,24 +1,72 @@
 import request from "../../utils/request.js"
 Page({
   data: {
-    list:[],
-    getIndex:0
+    hasMore: true,
+    keyword: '',
+    goods: [],
+    getIndex: 0,
+    pagenum: 1,
+    loading: false
   },
-  onLoad() {
-    request({
-      url:"/goods/search"
-    }).then(res=>{
-      const { message } = res.data
-      console.log(message)
-      this.setData({
-        list: message
-      })
-    })
-  },
-  handclick(e){
-    const { index } = e.currentTarget.dataset
+  onLoad: function(options) {
+      const {
+        keyword
+      } = options;
     this.setData({
-      getIndex : index
+      keyword
     })
+
+    this.getgoods();
+
+  },
+  getgoods() {
+    if (this.data.hasMore == false) {
+      return
+    }
+    request({
+      url: "/goods/search",
+      data: {
+        query: this.data.keyword,
+        pagenum: this.data.pagenum,
+        pagesize: 10
+      }
+    }).then(res => {
+      const {
+        message
+      } = res.data
+      console.log(message)
+      const goods = message.goods.map(v => {
+        v.goods_price = Number(v.goods_price).toFixed(2);
+        return v
+      })
+      this.setData({
+        goods: [...this.data.goods, ...goods],
+        loading: true
+      })
+      if (this.data.goods.length == message.total) {
+        hasMore = false
+      }
+    })
+  },
+  onReachBottom() {
+    if (this.data.loading == true) {
+
+      this.setData({
+        pagenum: this.data.pagenum + 1
+      })
+      this.getgoods();
+    }
+  },
+  handclick(e) {
+    const {
+      index
+    } = e.currentTarget.dataset
+    this.setData({
+      getIndex: index
+    })
+  },
+  clickinput() {
+
   }
+
 })
--- pages/search/index.js
+++ pages/search/index.js
@@ -1,66 +1,60 @@
 import request from "../../utils/request.js"
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    loading: '',
+    loadingvalue: '',
+    recommend: [],
+    list: false,
+  },
+  onLoad: function(options) {
 
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handinput(e) {
+    const {
+      value
+    } = e.detail
+    this.setData({
+      loading: value
+    })
+    if (!value) {
+      this.setData({
+        recommend: []
+      })
+      return
+    }
+    this.getrecommend()
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handclicknone() {
+    this.setData({
+      loading: '',
+      recommend: []
+    })
   },
+  getrecommend() {
+    if (this.data.list === false) {
+      this.setData({
+        list: true,
+        loadingvalue: this.data.loading
+      })
+      request({
+        url: '/goods/qsearch',
+        data: {
+          query: this.data.loading
+        }
+      }).then(res => {
+        const {
+          message
+        } = res.data
+        this.setData({
+          recommend: message,
+          list: false
+        })
+        if (this.data.loadingvalue !== this.data.loading) {
+          this.getrecommend()
+        }
+      })
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+    }
   }
+
 })
