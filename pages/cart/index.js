32bb7648a91cce977648a5ad0b1be27b8874cf4f
魔人布欧 <1067711620@qq.com>
2020-03-10 08:07:59 +0800

    18.03.01

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -5,12 +5,22 @@ Page({
    * 页面的初始数据
    */
   data: {
-    address: {}
+    address: {},
+    goods: [],
+    allprice: 0,
+    numner: 1
+
   },
   onLoad() {
     this.setData({
-      address: wx.getStorage("address") || {}
+      address: wx.getStorageSync("address") || {}
     })
+  },
+  onShow() {
+    this.setData({
+        goods: wx.getStorageSync('goods')
+      }),
+      this.handallprice();
   },
   handleGetAdd(e) {
     wx.chooseAddress({
@@ -31,9 +41,71 @@ Page({
           }
 
         })
-        wx.setStorageSync('address', this.data.address)
+        wx.setStorageSync("address", this.data.address)
       }
     })
+  },
+  handallprice() {
+    let price = 0;
+    this.data.goods.forEach(v => {
+      if(v.selete){
+        price += v.goods_price * v.number
+      }
+    })
+    this.setData({
+      allprice: price,
+    })
+    wx.setStorageSync('goods', this.data.goods)
+  },
+  handnumber(e) {
+    const {
+      index,
+      number
+    } = e.currentTarget.dataset
+    this.data.goods[index].number += number
+    if (this.data.goods[index].number === 0) {
+      wx.showModal({
+        title: '提示',
+        content: '是否删除',
+        success: (res) => {
+          if (res.confirm) {
+            this.data.goods.splice(index, 1)
+          } else {
+            this.data.goods[index].number += 1
+          }
+          this.setData({
+            goods: this.data.goods
+          })
+         
+        }
+      })
+    }
+    this.setData({
+      goods: this.data.goods
+    })
+    this.handallprice();
+  },
+  handblur(e){
+    const { index}= e.currentTarget.dataset;
+    let {value} = e.detail
+    value = Math.floor(Number(value))
+    if(value <= 0){
+      value = 1
+    }
+    this.data.goods[index].number = Number(value)
+    console.log(e)
+    this.setData({
+      goods: this.data.goods
+    })
+    this.handallprice();
+  },
+  handclickicon(e){
+    const { index } = e.currentTarget.dataset;
+    const { selete } = this.data.goods[index];
+    this.data.goods[index].selete = !selete
+    this.setData({
+      goods: this.data.goods
+    })
+    this.handallprice();
   }
-
 })
--- pages/goods_detail/index.js
+++ pages/goods_detail/index.js
@@ -2,19 +2,90 @@ import request from "../../utils/request.js"
 Page({
 
   data: {
-    background: ['demo-text-1', 'demo-text-2', 'demo-text-3'],
     indicatorDots: true,
     vertical: false,
     autoplay: false,
     interval: 2000,
-    duration: 500
-    
+    duration: 500,
+    detail: [],
+    tab: 0,
+    current: [], // 当前显示图片的http链接
+    urls: [],
+    goods: []
+
   },
-  onLoad(){
+  onLoad: function(options) {
+    console.log(options)
     request({
-      url:"/goods/detail"
-    }).then(res=>{
+      url: "/goods/detail",
+      data: {
+        goods_id: options.id
+      }
+    }).then(res => {
       console.log(res.data)
+      const {
+        message
+      } = res.data;
+      const pics = message.pics.map(v => {
+        return v.pics_big
+      })
+      this.setData({
+        detail: message,
+        urls: pics
+
+      })
     })
+  },
+
+  handclicktab(e) {
+
+    const {
+      index
+    } = e.currentTarget.dataset
+    this.setData({
+      tab: index
+    })
+  },
+  handclickimage(e) {
+    const {
+      index
+    } = e.currentTarget.dataset
+    wx.previewImage({
+      current: this.data.urls[index], // 当前显示图片的http链接
+      urls: this.data.urls // 需要预览的图片http链接列表
+    })
+  },
+  handsechitab(e) {
+    wx.switchTab({
+      url: '/pages/cart/index'
+    })
+  },
+  handshopping() {
+    const goods = wx.getStorageSync("goods") || []
+    const exit = goods.some(v => {
+      if (this.data.detail.goods_id === v.goods_id) {
+        v.number += 1
+        wx.showToast({
+          title: '商品+1',
+          icon: 'success',
+          duration: 2000
+        })
+      }
+      return this.data.detail.goods_id === v.goods_id
+    })
+
+    if (exit == false) {
+      goods.unshift({
+        goods_id: this.data.detail.goods_id,
+        goods_name: this.data.detail.goods_name,
+        goods_price: this.data.detail.goods_price,
+        goods_small_logo: this.data.detail.goods_small_logo,
+        number: 1,
+        selete:true
+      })
+    }
+    console.log(exit)
+
+    wx.setStorageSync("goods", goods)
   }
 })
